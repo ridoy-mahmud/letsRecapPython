31b24dd2d4812d74dcb581b879eb4a32264f6a62
ridoy-mahmud <ridoymahmud678@gmail.com>
2025-02-13 01:06:14 +0600

    Updated

---
+++ Python Basics/function_basic.py
@@ -0,0 +1,9 @@
+def add(num1 , num2):
+    return num1+num2
+res = add(50,30)
+print(f"The result is - {res}")
+
+def subtruction (num1 , num2):
+    return num1-num2
+res2 = subtruction(50,30)
+print(f"The subtruction is - {res2}")
